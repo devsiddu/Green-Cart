b32b1575a7881277b370fae70b7e2d3766222965
devsiddu <siddu124123@gmail.com>
2025-04-17 20:02:11 +0530

    project completed

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -51,7 +51,7 @@ const MyOrders = () => {
                             </div>
                             <div className='flex flex-col text-primary justify-center md:ml-8 mb-4 md:mb-0'>
                                 <p>Quantity : {item.quantity || "1"}</p>
-                                <p>Status : {item.status}</p>
+                                <p>Status : {order.status}</p>
                                 <p>Date : {new Date(order.createdAt).toLocaleDateString()}</p>
                             </div>
                             <p className='text-primary text-lg font-medium'>
